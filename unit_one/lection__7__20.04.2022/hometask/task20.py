826795743aaf680af8396ead7814eca4cbc193e8
PavelMoshchenkov <pmy.spb@gmail.com>
2022-04-27 11:44:06 +0300

    20 задача готова

---
+++ unit_one/lection__7__20.04.2022/hometask/task20.py
@@ -0,0 +1,19 @@
+# Выведите все строки данного файла в обратном порядке.
+# Для этого считайте список всех строк при помощи метода readlines().
+
+# Содержимое файла import_this.txt
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+#
+# выходные данные
+# Complex is better than complicated.
+# Simple is better than complex.
+# Explicit is better than implicit.
+# Beautiful is better than ugly.
+
+file = open("import_this.txt", "r", encoding="utf-8")
+rev = file.readlines()
+rev.reverse()
+print("".join(rev).strip('\n'))
